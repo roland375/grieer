b4d702f02a036a371046aa14047f4ea9c4a0ee29
roland375 <roland@grieer.com>
2026-05-26 09:27:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,4 @@
-// === GRIEER SW - NIE CACHEN ===
-self.addEventListener('install', function(e){ self.skipWaiting(); });
-self.addEventListener('activate', function(e){
-  e.waitUntil(
-    caches.keys().then(function(names){
-      return Promise.all(names.map(function(n){ return caches.delete(n); }));
-    }).then(function(){ return self.clients.claim(); })
-  );
-});
-self.addEventListener('fetch', function(e){
-  // IMMER frisch laden, NIE aus Cache
-  e.respondWith(
-    fetch(e.request, {cache: 'no-store'}).catch(function(){
-      return new Response('Offline', {status: 503});
-    })
-  );
-});
+// Grieer SW 20260525160134
+self.addEventListener('install',function(e){self.skipWaiting();});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(n){return caches.delete(n);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(function(){return new Response('Offline',{status:503});}));});
