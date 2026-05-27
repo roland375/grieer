c44b11e065ef9d4fef0d17c6d0c4099fbf148228
roland375 <roland@grieer.com>
2026-05-27 07:00:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// Grieer SW 20260525160134
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(n){return caches.delete(n);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(function(){return new Response('Offline',{status:503});}));});
