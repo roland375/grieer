d156e3e6eea096c52e0921085fd8ffe63ed85a1e
roland375 <roland@grieer.com>
2026-05-28 07:55:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-self.addEventListener('install',function(e){self.skipWaiting();});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(n){return caches.delete(n);}));}).then(function(){return self.clients.claim();}));});
-self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(function(){return new Response('Offline',{status:503});}));});
+self.addEventListener('install',function(e){self.skipWaiting();});self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(n){return caches.delete(n);}))}).then(function(){return self.clients.claim();}));});self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(function(){return new Response('Offline',{status:503});}));});
