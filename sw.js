1493f5f420d60862d7b1b16dd90e1ce904506c96
roland375 <roland@grieer.com>
2026-06-08 06:55:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-// Grieer SW v20260603091830 - kein Cache, immer aktuell
-self.addEventListener('install',e=>{e.waitUntil(self.skipWaiting());});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('install',e=>{e.waitUntil(self.skipWaiting());});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>caches.delete(n)))).then(()=>self.clients.claim()));});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
